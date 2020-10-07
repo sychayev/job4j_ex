8f94814983f6dae8381a01e6ab7214e8aa250dc1
petrs <sychayev.petr>
2020-10-07 17:49:31 +0300

    0. Что такое исключение.

--- src/ex/BackArray.java
+++ src/ex/BackArray.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 public class BackArray {
     public static void main(String[] args) {
 
-        String [] names = {"Petr", "Ivan", "Nik", "Vasya"};
+        String[] names = {"Petr", "Ivan", "Nik", "Vasya"};
         int middle = names.length / 2;
-        for(int i = 0;i < middle;i++) {
+        for (int i = 0; i < middle - 1; i++) {
             String temp = names[i];
-            names[i] = names[names.length-1];
-            names[names.length - 1] = temp;
+            names[i] = names[names.length + i - 1];
+            names[names.length + i - 1] = temp;
         }
 
         System.out.println(Arrays.toString(names));
