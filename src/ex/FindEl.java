2d52a280232515b56fd64b421befd02d542751e3
petrs <sychayev.petr>
2020-10-19 20:45:23 +0300

    0.4. Пользовательские исключения.

--- src/ex/FindEl.java
+++ src/ex/FindEl.java
@@ -1,25 +1,28 @@
 package ex;
 
 import javax.management.StringValueExp;
+import java.util.Arrays;
 
 public class FindEl {
     public static int indexOf(String[] value, String key)
             throws ElementNotFoundException {
         int rsl = -1;
         for (String s : value) {
-        if (s !="b"){
-            System.out.println();
-        }
+            if (null == value) {
+                throw new ElementNotFoundException("Element not found!");
+            } else {
+                System.out.println(s);
+            }
         }
         return rsl;
     }
 
     public static void main(String[] args)
             throws ElementNotFoundException {
-        String s [] = new String []{"a","b","c",null};
-        try{
-            FindEl.indexOf(s,"b");
-        }catch(ElementNotFoundException e){
+        String s1[] = new String[]{"a", "b", "c", null};
+        try {
+            FindEl.indexOf(s1, null);
+        } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }
     }
